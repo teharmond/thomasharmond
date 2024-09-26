32824f27c0dca7a577b810a6323b92c16fababe9
teharmond <teharmond@gmail.com>
2024-09-26 12:41:26 -0500

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  return <div className="">Hello.</div>;
+  return (
+    <div className="">
+      Hello.
+      <br />
+      <br />
+      Currently building <Link href="https://trivo.app">Trivo</Link>
+    </div>
+  );
 }
